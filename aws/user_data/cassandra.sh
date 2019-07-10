1a0d37a7c741cfdc7bf7e21ee67a720228147d40
Quinlan Carlos-Shanley <quin.shanley@gmail.com>
2019-07-10 09:26:00 -0500

    Delete cassandra.sh

--- aws/user_data/cassandra.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-roll=cassandra-scale-test
-cd /tmp
-netstat -nr > /tmp/netstat.out
-ping -c 20 www.google.com > /tmp/ping_google.out
-apt-get update -y; apt-get upgrade -y; apt-get install -y unzip
-wget https://github.com/qjshanley/cassandra_scale_test/archive/master.zip
-unzip master.zip
-bash cassandra_scale_test*/cassandra_bootstrap
